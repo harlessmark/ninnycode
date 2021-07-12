5df812dd72d97931cbcc3b518a45c062aed7c13e
Mark Harless <59143065+harlessmark@users.noreply.github.com>
2021-07-12 10:34:49 -0400

    Update Footer.js

--- src/styled/Footer.js
+++ src/styled/Footer.js
@@ -9,16 +9,6 @@ function Footer() {
   return (
     <footer>
       <P footerText>
-        <A href='https://buymeacoffee.com/2spacemilk' rel='noopener noreferrer'>
-          <strong>Buy Me a Coffee</strong>
-          <i
-            className='far fa-coffee-togo animate__animated animate__delay-5s animate__rotateOutUpRight'
-            style={{ marginLeft: "5px" }}
-          />
-        </A>
-      </P>
-
-      <P footerText>
         <Link to='/privacy' className='strip-decoration'>
           Privacy Policy
           <br />
